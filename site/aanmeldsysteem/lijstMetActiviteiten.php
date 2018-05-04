74939001bec9b0ae7fbb90eac3d9b2957bf1182f
jordy-u <16016998@student.hhs.nl>
2018-05-04 20:58:35 +0200

    Maand kiezen in maandoverzicht

--- site/aanmeldsysteem/lijstMetActiviteiten.php
+++ site/aanmeldsysteem/lijstMetActiviteiten.php
@@ -2,16 +2,30 @@
 <?php
 include('mysqli_connect.php');
 //$query = "SELECT id, datum, type FROM activiteit ORDER BY datum ASC";
-$query = "SELECT id, DATE_FORMAT(datum, '%d %M'), type, titel FROM activiteit ORDER BY datum ASC";
-$stmt = $dbc->prepare($query);
+$jaar =  (isset($_GET['jaar'] )) ? $_GET['jaar'] : date("Y");
+$maand = (isset($_GET['maand'])) ? $_GET['maand'] : date("n");
+
+if(isset($_GET['alle-meldingen'])) {
+	$query = "SELECT id, DATE_FORMAT(datum, '%d %M'), type, titel FROM activiteit ORDER BY datum ASC";
+	$stmt = $dbc->prepare($query);
+} else  {
+	//$query = "SELECT id, DATE_FORMAT(datum, '%d %M'), type, titel FROM activiteit ORDER BY datum ASC WHERE MONTH(happened_at) = 5 AND YEAR(happened_at) = ?";
+	$query = "SELECT id, datum, DATE_FORMAT(datum, '%d %M'), type, titel FROM activiteit WHERE MONTH(datum)=? AND YEAR(datum)=? ORDER BY datum ASC";
+	$stmt = $dbc->prepare($query);
+	$stmt->bind_param("ss", $maand, $jaar);
+			
+}
+
+
 $stmt->execute();
 $response = $stmt->get_result();
 if($response){
-	
-  
+	$activiteit = [];
+  $minstensEenActiviteit = false;
   $aantalActiviteiten = 0;
 	//Zet Activiteiten in een list.
 	while($row = mysqli_fetch_array($response)){
+		$minstensEenActiviteit = true;
 		$activiteit[$aantalActiviteiten][0] = $row['id'];
 		$activiteit[$aantalActiviteiten][1] = $row["DATE_FORMAT(datum, '%d %M')"];
 		$activiteit[$aantalActiviteiten][2] = $row['type'];
--- site/aanmeldsysteem/maandoverzicht.php
+++ site/aanmeldsysteem/maandoverzicht.php
@@ -24,7 +24,7 @@
 		border: 1px solid gray;
 		border-collapse: collapse;
 	}
-	.legenda {
+	.noBorder {
 		border: 0px solid yellow;
 		border-collapse: separate;
 	}
@@ -33,6 +33,8 @@
 	<body style="background-color:Black;">
 	<link rel="stylesheet" type="text/css" href="/style.css">
 	<h2><a href="http://chaos2003.nl">❰ Index</a></h2>
+	
+
 
 
 
@@ -41,9 +43,43 @@
 require_once('inloggen.php');
 //Zet Chaoten in lijst
 if ($chaoot_ingelogd) {
-	echo '<h1>Maandoverzicht</h1>
+	echo '<h1>Maandoverzicht DIT IS NIET DE GOEDE SITE. GA NAAR <a href="http://www.chaos2003.nl">chaos2003.nl</a></h1>
 	<p><a style="color:cyan;" href="maakActiviteitAan.php">Klik hier om een nieuwe activiteit toe te voegen.</a><br><br>
 	Druk op een datum om naar die activiteit te springen.</p>';
+	
+	//Jaar en Maand kiezen om meldingen te zien van een andere periode
+	$jaar =  (isset($_GET['jaar'] )) ? $_GET['jaar'] : date("Y");
+	$maand = (isset($_GET['maand'])) ? $_GET['maand'] : date("n");
+	echo '<form action="maandoverzicht" method="get">
+		 <table class="noBorder">
+		 <tr class="noBorder">
+		   <td class="noBorder">Jaar</td>
+		   <td class="noBorder"><input type="text" name="jaar" size="10" value="' . $jaar . '"></td>
+		 </tr>
+		 <tr class="noBorder">
+		   <td class="noBorder">Maand&nbsp;&nbsp;&nbsp;</td>
+		   <td class="noBorder">
+			 <select name="maand" id="maand">';
+			echo '<option value="1"  '; if ($maand == 1 ) echo "selected"; echo '>Januari</option>';
+			echo '<option value="2"  '; if ($maand == 2 ) echo "selected"; echo '>Februari</option>';
+			echo '<option value="3"  '; if ($maand == 3 ) echo "selected"; echo '>Maart</option>';
+			echo '<option value="4"  '; if ($maand == 4 ) echo "selected"; echo '>April</option>';
+			echo '<option value="5"  '; if ($maand == 5 ) echo "selected"; echo '>Mei</option>';
+			echo '<option value="6"  '; if ($maand == 6 ) echo "selected"; echo '>Juni</option>';
+			echo '<option value="7"  '; if ($maand == 7 ) echo "selected"; echo '>Juli</option>';
+			echo '<option value="8"  '; if ($maand == 8 ) echo "selected"; echo '>Augustus</option>';
+			echo '<option value="9"  '; if ($maand == 9 ) echo "selected"; echo '>September</option>';
+			echo '<option value="10" '; if ($maand == 10) echo "selected"; echo '>Oktober</option>';
+			echo '<option value="11" '; if ($maand == 11) echo "selected"; echo '>November</option>';
+			echo '<option value="11" '; if ($maand == 12) echo "selected"; echo '>December</option>';
+			echo '</select>
+		   </td>
+		 </tr>
+		 </table>
+		 <input type="submit" value="Bekijk maand">
+	</form>';
+	
+	
 	include('lijstMetChaoten.php');
 	echo '<br><br>';
 	require('lijstMetActiviteiten.php');
@@ -51,13 +87,13 @@ if ($chaoot_ingelogd) {
 	require('lijstMetMeldingen.php');
 	
 	echo '<p><b>Legenda:</b><br>
-	<table class="legenda">
+	<table class="noBorder">
 	
-	<tr class="legenda"><td class="legenda">							Zwart:		</td><td class="legenda">Niks ingevoerd</td>
-	<tr class="legenda"><td class="legenda"><a style="color:lime;">		Groen:	</a></td><td class="legenda">Gaat borrelen</td>
-	<tr class="legenda"><td class="legenda"><a style="color:yellow;">	Geel:	</a></td><td class="legenda">Komt eten, maar gaat niet borrelen</td>
-	<tr class="legenda"><td class="legenda"><a style="color:red;">		Rood:	</a></td><td class="legenda">Is afwezig</td>
-	<tr class="legenda"><td class="legenda"><a style="color:gray;">		Grijs:	</a></td><td class="legenda">Turft niet op Chaos</td>
+	<tr class="noBorder"><td class="noBorder">							Zwart:		</td><td class="noBorder">Niks ingevoerd</td>
+	<tr class="noBorder"><td class="noBorder"><a style="color:lime;">		Groen:	</a></td><td class="noBorder">Gaat borrelen</td>
+	<tr class="noBorder"><td class="noBorder"><a style="color:yellow;">	Geel:	</a></td><td class="noBorder">Komt eten, maar gaat niet borrelen</td>
+	<tr class="noBorder"><td class="noBorder"><a style="color:red;">		Rood:	</a></td><td class="noBorder">Is afwezig</td>
+	<tr class="noBorder"><td class="noBorder"><a style="color:gray;">		Grijs:	</a></td><td class="noBorder">Turft niet op Chaos</td>
 	</table>
 	</p>
 	';
